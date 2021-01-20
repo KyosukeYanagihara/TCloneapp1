e2f507f9b269a87ffb7176c801d7268cc0c1ef39
KyosukeYanagihara <rctttzg7@gmail.com>
2021-01-20 15:42:15 +0900

    つぶやき機能構築

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,11 +1,24 @@
 class WordsController < ApplicationController
+  before_action :set_word, only: [:show, :edit, :update, :destroy]
   def index
+    @words = Word.all
   end
 
   def new
+    @word = Word.new
   end
 
   def create
+    @word = Word.new(word_params)
+    if params[:back]
+      render :new
+      else
+      if @word.save
+        redirect_to words_path, notice: "つぶやきました！"
+        else
+        render :new
+      end
+    end
   end
 
   def show
@@ -15,9 +28,29 @@ class WordsController < ApplicationController
   end
 
   def update
+    if @word.update(word_params)
+      redirect_to words_path, notice: "ブログを編集しました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @word.destroy
+    redirect_to words_path, notice: "削除しました。"
   end
 
+  def confirm
+    @word = Word.new(word_params)
+    render :new if @word.invalid?
+  end
+  
+  private
+  def word_params
+    params.require(:word).permit(:content)
+  end
+
+  def set_word
+    @word = Word.find(params[:id])
+  end
 end
